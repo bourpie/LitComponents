ef703be557da47bf635038ae5037a7141ee6930f
bourpie <pbcomweb@gmail.com>
2022-05-21 11:34:39 -0400

    3 files modified, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export * from './my-element.js';
-export * from './my-hamburger.js';
-export * from './my-hamburger-menu.js';
+export * from './my-element';
+export * from './my-hamburger';
+export * from './my-hamburger';
