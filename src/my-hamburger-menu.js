f9f1f3b5ef1c663f039235ba0d50c2eadf2f1518
bourpie <pbcomweb@gmail.com>
2022-05-21 11:09:57 -0400

    2 files modified

--- src/my-hamburger-menu.js
+++ src/my-hamburger-menu.js
@@ -1,17 +1,6 @@
 import { LitElement, html } from 'lit';
 
 export class MyHamburgerMenu extends LitElement {
-  static get styles() {
-    return css`
-
-      :host {
-        --my-hamburger-menu-display: none;
-      }
-      div {
-      display: var(--my-hamburger-menu-display);
-      }
-    `;
-  }
   render() {
     return html`
     <div>
